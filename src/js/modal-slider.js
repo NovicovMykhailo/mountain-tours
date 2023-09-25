83746bb18b0503db9362863c210275b44d466478
NovicovMykhailo <mikenovicov@gmail.com>
2023-09-25 20:32:05 +0300

    try to fix images vol 2

--- src/js/modal-slider.js
+++ src/js/modal-slider.js
@@ -1,11 +1,11 @@
-$('.modal-slider').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  initialSlide: 0,
-  arrows: false,
-  dots: true,
+// $('.modal-slider').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   initialSlide: 0,
+//   arrows: false,
+//   dots: true,
 
-  customPaging: function (slider, i) {
-    return "<span class='dot'></span>";
-  },
-});
+//   customPaging: function (slider, i) {
+//     return "<span class='dot'></span>";
+//   },
+// });
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,15 +1,27 @@
-$('.gallery-list').slick({
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  arrows: true,
-  dots: false,
-  initialSlide: 4,
-  appendArrows: '.slider-arrows',
-  prevArrow: '.left',
-  nextArrow: '.right',
+$(window).on('load', function () {
+
+  $('.modal-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    arrows: false,
+    dots: true,
+  
+    customPaging: function (slider, i) {
+      return "<span class='dot'></span>";
+    },
+  });
+
+
+  $('.gallery-list').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    initialSlide: 4,
+    appendArrows: '.slider-arrows',
+    prevArrow: '.left',
+    nextArrow: '.right',
+  });
 });
-
-
-
-
 
