49a66aa0ff090e0f0b8ec50f433c9f2e44603e87
NovicovMykhailo <mikenovicov@gmail.com>
2023-09-26 15:45:51 +0300

    update gallery var 4

---
+++ src/js/photoImport.js
@@ -0,0 +1,18 @@
+
+
+import Img1 from '../img/photos/gallery/lg-dsk/gallerygal@2x.jpg';
+import Img2 from '../img/photos/gallery/lg-dsk/gallerygal@2x-1.jpg';
+import Img3 from '../img/photos/gallery/lg-dsk/gallerygal@2x-2.jpg';
+import Img4 from '../img/photos/gallery/lg-dsk/gallerygal@2x-3.jpg';
+import Img5 from '../img/photos/gallery/lg-dsk/gallerygal@2x-4.jpg';
+import Img6 from '../img/photos/gallery/lg-dsk/gallerygal@2x-5.jpg';
+import Img7 from '../img/photos/gallery/lg-dsk/gallerygal@2x-6.jpg';
+import Img8 from '../img/photos/gallery/lg-dsk/gallerygal@2x-7.jpg';
+
+export default {
+  data() {
+    return {
+      images: [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8],
+    };
+  },
+};
