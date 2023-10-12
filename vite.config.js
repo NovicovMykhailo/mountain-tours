b31990a9944754a7297da3b7359e938fe92d5020
NovicovMykhailo <mikenovicov@gmail.com>
2023-10-12 15:09:18 +0300

    update sprite imnplimenta

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import ViteSvgSpriteWrapper from 'vite-svg-sprite-wrapper';
+
 
 export default defineConfig({
   root: 'src',
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   plugins: [
     injectHTML(),
-    ViteSvgSpriteWrapper({ icons: 'src/img/*.svg', outputDir: 'src/public' }),
+
     FullReload(['./src/**/**.html']),
   ],
 });
